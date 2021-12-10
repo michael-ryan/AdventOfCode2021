cef5abc34b310474cc4dbb313144e45b1089d60a
mehmenmike <mehmenmike@gmail.com>
2021-12-10 12:14:50 +0000

    Add time reporting

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,6 @@
+import java.time.Duration;
+import java.time.Instant;
+
 public class Main {
     /*
     Increase this int
@@ -5,9 +8,11 @@ public class Main {
     Uncomment around line 100
      */
     private static final int PUZZLES_DONE = 10;
+    private int day = 1;
+    private int puzzle = 1;
+    private Instant now;
 
-
-    public static void main(String[] args){
+    private void go(){
         Number[][] answers = generateAnswerArray();
 
         if(answersCorrect(answers)){
@@ -16,6 +21,10 @@ public class Main {
                 printAnswers(i + 1, answers[i][0], answers[i][1]);
             }
         }
+    }
+
+    public static void main(String[] args){
+        new Main().go();
     }
 
     private static boolean answersCorrect(Number[][] actual){
@@ -75,63 +84,78 @@ public class Main {
         System.out.println(" - Puzzle two: " + answerTwo);
     }
 
-    private static Number[][] generateAnswerArray(){
+    private Number[][] generateAnswerArray(){
         Number[][] answers = new Number[25][];
         for(int i = 0; i < answers.length; i++){
             answers[i] = new Number[2];
         }
 
-        answers[0][0] = new Day1.Puzzle1.Solution().run();
-        answers[0][1] = new Day1.Puzzle2.Solution().run();
-        answers[1][0] = new Day2.Puzzle1.Solution().run();
-        answers[1][1] = new Day2.Puzzle2.Solution().run();
-        answers[2][0] = new Day3.Puzzle1.Solution().run();
-        answers[2][1] = new Day3.Puzzle2.Solution().run();
-        answers[3][0] = new Day4.Puzzle1.Solution().run();
-        answers[3][1] = new Day4.Puzzle2.Solution().run();
-        answers[4][0] = new Day5.Puzzle1.Solution().run();
-        answers[4][1] = new Day5.Puzzle2.Solution().run();
-        answers[5][0] = new Day6.Puzzle1.Solution().run();
-        answers[5][1] = new Day6.Puzzle2.Solution().run();
-        answers[6][0] = new Day7.Puzzle1.Solution().run();
-        answers[6][1] = new Day7.Puzzle2.Solution().run();
-        answers[7][0] = new Day8.Puzzle1.Solution().run();
-        answers[7][1] = new Day8.Puzzle2.Solution().run();
-        answers[8][0] = new Day9.Puzzle1.Solution().run();
-        answers[8][1] = new Day9.Puzzle2.Solution().run();
-        answers[9][0] = new Day10.Puzzle1.Solution().run();
-        answers[9][1] = new Day10.Puzzle2.Solution().run();
-/*        answers[10][0] = new Day11.Puzzle1.Solution().run();
-        answers[10][1] = new Day11.Puzzle2.Solution().run();
-        answers[11][0] = new Day12.Puzzle1.Solution().run();
-        answers[11][1] = new Day12.Puzzle2.Solution().run();
-        answers[12][0] = new Day13.Puzzle1.Solution().run();
-        answers[12][1] = new Day13.Puzzle2.Solution().run();
-        answers[13][0] = new Day14.Puzzle1.Solution().run();
-        answers[13][1] = new Day14.Puzzle2.Solution().run();
-        answers[14][0] = new Day15.Puzzle1.Solution().run();
-        answers[14][1] = new Day15.Puzzle2.Solution().run();
-        answers[15][0] = new Day16.Puzzle1.Solution().run();
-        answers[15][1] = new Day16.Puzzle2.Solution().run();
-        answers[16][0] = new Day17.Puzzle1.Solution().run();
-        answers[16][1] = new Day17.Puzzle2.Solution().run();
-        answers[17][0] = new Day18.Puzzle1.Solution().run();
-        answers[17][1] = new Day18.Puzzle2.Solution().run();
-        answers[18][0] = new Day19.Puzzle1.Solution().run();
-        answers[18][1] = new Day19.Puzzle2.Solution().run();
-        answers[19][0] = new Day20.Puzzle1.Solution().run();
-        answers[19][1] = new Day20.Puzzle2.Solution().run();
-        answers[20][0] = new Day21.Puzzle1.Solution().run();
-        answers[20][1] = new Day21.Puzzle2.Solution().run();
-        answers[21][0] = new Day22.Puzzle1.Solution().run();
-        answers[21][1] = new Day22.Puzzle2.Solution().run();
-        answers[22][0] = new Day23.Puzzle1.Solution().run();
-        answers[22][1] = new Day23.Puzzle2.Solution().run();
-        answers[23][0] = new Day24.Puzzle1.Solution().run();
-        answers[23][1] = new Day24.Puzzle2.Solution().run();
-        answers[24][0] = new Day25.Puzzle1.Solution().run();
-        answers[24][1] = new Day25.Puzzle2.Solution().run();
+        now = Instant.now();
+        answers[0][0] = new Day1.Puzzle1.Solution().run(); reportTime();
+        answers[0][1] = new Day1.Puzzle2.Solution().run(); reportTime();
+        answers[1][0] = new Day2.Puzzle1.Solution().run(); reportTime();
+        answers[1][1] = new Day2.Puzzle2.Solution().run(); reportTime();
+        answers[2][0] = new Day3.Puzzle1.Solution().run(); reportTime();
+        answers[2][1] = new Day3.Puzzle2.Solution().run(); reportTime();
+        answers[3][0] = new Day4.Puzzle1.Solution().run(); reportTime();
+        answers[3][1] = new Day4.Puzzle2.Solution().run(); reportTime();
+        answers[4][0] = new Day5.Puzzle1.Solution().run(); reportTime();
+        answers[4][1] = new Day5.Puzzle2.Solution().run(); reportTime();
+        answers[5][0] = new Day6.Puzzle1.Solution().run(); reportTime();
+        answers[5][1] = new Day6.Puzzle2.Solution().run(); reportTime();
+        answers[6][0] = new Day7.Puzzle1.Solution().run(); reportTime();
+        answers[6][1] = new Day7.Puzzle2.Solution().run(); reportTime();
+        answers[7][0] = new Day8.Puzzle1.Solution().run(); reportTime();
+        answers[7][1] = new Day8.Puzzle2.Solution().run(); reportTime();
+        answers[8][0] = new Day9.Puzzle1.Solution().run(); reportTime();
+        answers[8][1] = new Day9.Puzzle2.Solution().run(); reportTime();
+        answers[9][0] = new Day10.Puzzle1.Solution().run(); reportTime();
+        answers[9][1] = new Day10.Puzzle2.Solution().run(); reportTime();
+/*        answers[10][0] = new Day11.Puzzle1.Solution().run(); reportTime();
+        answers[10][1] = new Day11.Puzzle2.Solution().run(); reportTime();
+        answers[11][0] = new Day12.Puzzle1.Solution().run(); reportTime();
+        answers[11][1] = new Day12.Puzzle2.Solution().run(); reportTime();
+        answers[12][0] = new Day13.Puzzle1.Solution().run(); reportTime();
+        answers[12][1] = new Day13.Puzzle2.Solution().run(); reportTime();
+        answers[13][0] = new Day14.Puzzle1.Solution().run(); reportTime();
+        answers[13][1] = new Day14.Puzzle2.Solution().run(); reportTime();
+        answers[14][0] = new Day15.Puzzle1.Solution().run(); reportTime();
+        answers[14][1] = new Day15.Puzzle2.Solution().run(); reportTime();
+        answers[15][0] = new Day16.Puzzle1.Solution().run(); reportTime();
+        answers[15][1] = new Day16.Puzzle2.Solution().run(); reportTime();
+        answers[16][0] = new Day17.Puzzle1.Solution().run(); reportTime();
+        answers[16][1] = new Day17.Puzzle2.Solution().run(); reportTime();
+        answers[17][0] = new Day18.Puzzle1.Solution().run(); reportTime();
+        answers[17][1] = new Day18.Puzzle2.Solution().run(); reportTime();
+        answers[18][0] = new Day19.Puzzle1.Solution().run(); reportTime();
+        answers[18][1] = new Day19.Puzzle2.Solution().run(); reportTime();
+        answers[19][0] = new Day20.Puzzle1.Solution().run(); reportTime();
+        answers[19][1] = new Day20.Puzzle2.Solution().run(); reportTime();
+        answers[20][0] = new Day21.Puzzle1.Solution().run(); reportTime();
+        answers[20][1] = new Day21.Puzzle2.Solution().run(); reportTime();
+        answers[21][0] = new Day22.Puzzle1.Solution().run(); reportTime();
+        answers[21][1] = new Day22.Puzzle2.Solution().run(); reportTime();
+        answers[22][0] = new Day23.Puzzle1.Solution().run(); reportTime();
+        answers[22][1] = new Day23.Puzzle2.Solution().run(); reportTime();
+        answers[23][0] = new Day24.Puzzle1.Solution().run(); reportTime();
+        answers[23][1] = new Day24.Puzzle2.Solution().run(); reportTime();
+        answers[24][0] = new Day25.Puzzle1.Solution().run(); reportTime();
+        answers[24][1] = new Day25.Puzzle2.Solution().run(); reportTime();
 */
         return answers;
+    }
+
+    private void reportTime(){
+        System.out.println("Day " + day + ", puzzle " + puzzle + " took "
+                + Duration.between(this.now, Instant.now()).toMillis() + "ms.");
+
+        if(this.puzzle == 2){
+            this.day++;
+            this.puzzle = 1;
+        } else {
+            this.puzzle++;
+        }
+
+        this.now = Instant.now();
     }
 }
